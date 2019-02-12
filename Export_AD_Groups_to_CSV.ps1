23cffcb63aed9ddb3cf572d9ed0ccd498749ccca
Ivanov <32060944+seyo-IV@users.noreply.github.com>
2019-02-12 11:05:35 +0100

    Add files via upload
    Original Author Victor Ashiedu

---
+++ Export_AD_Groups_to_CSV.ps1
@@ -0,0 +1,53 @@
+###########################################################
+# AUTHOR  				: Sergiy Ivanov 
+# Original Author		: Victor Ashiedu
+# CREATED 				: 08-08-2014 
+# UPDATED 				: 12-02-2019 
+# COMMENT 				: This script exports Active Directory groups
+#           to a a csv file.
+###########################################################
+
+
+#Define location of my script variable
+#the -parent switch returns one directory lower from directory defined. 
+#below will return up to ImportADUsers folder 
+#and since my files are located here it will find it.
+#It failes withpout appending "*.*" at the end
+
+$path = Split-Path -parent "C:\your\path\*.*"
+
+#Create a variable for the date stamp in the log file
+
+$LogDate = get-date -f yyyyMMddhhmm
+
+#Define CSV and log file location variables
+#they have to be on the same location as the script
+
+$csvfile = $path + "\ALLADGroups_$logDate.csv"
+
+#import the ActiveDirectory Module
+
+Import-Module ActiveDirectory
+
+
+#Sets the OU to do the base search for all user accounts, change as required.
+#Simon discovered that some users were missing
+#I decided to run the report from the root of the domain
+
+$SearchBase = "OU=Group,DC=example,DC=com"
+
+
+
+
+$AllADGroups = Get-ADGroup  -searchbase $SearchBase -Filter * -Properties * #| Where-Object {$_.info -NE 'Migrated'} #ensures that updated users are never exported.
+
+$AllADGroups |
+Select-Object @{Label = "sAMAccountName";Expression = {$_.sAMAccountName}},
+@{Label = "Description";Expression = {$_.description}},
+@{Label = "MemberOf";Expression = {foreach($group in $AllADGroups){$ADGroup = Get-ADGroup -Identity $group -Properties memberof | Select-Object -ExpandProperty memberof
+foreach($grp in $ADGroup){(Get-ADGroup -Filter * -SearchBase "$grp").sAMAccountName}}}},
+@{Label = "Member sAMAccountName";Expression = {foreach($group in $AllADGroups){$ADGroup = Get-ADGroup -Identity $group -Properties member | Select-Object -ExpandProperty member
+foreach($user in $ADGroup){(Get-ADUser -Filter * -SearchBase "$user").sAMAccountName}}}} |
+#Export CSV report
+
+Export-Csv -Path $csvfile -NoTypeInformation
---
+++ Export_AD_Users_to_CSV.ps1
@@ -0,0 +1,63 @@
+###########################################################
+# AUTHOR  				: Sergiy Ivanov 
+# Original Author		: Victor Ashiedu
+# CREATED 				: 08-08-2014 
+# UPDATED 				: 12-02-2019 
+# COMMENT 				: This script exports Active Directory user
+#           to a a csv file.
+###########################################################
+
+
+#Define location of my script variable
+#the -parent switch returns one directory lower from directory defined. 
+#below will return up to ImportADUsers folder 
+#and since my files are located here it will find it.
+#It failes withpout appending "*.*" at the end
+
+$path = Split-Path -parent "C:\your\path\*.*"
+
+#Create a variable for the date stamp in the log file
+
+$LogDate = get-date -f yyyyMMddhhmm
+
+#Define CSV and log file location variables
+#they have to be on the same location as the script
+
+$csvfile = $path + "\ALLADUsers_$logDate.csv"
+
+#import the ActiveDirectory Module
+
+Import-Module ActiveDirectory
+
+
+#Sets the OU to do the base search for all user accounts, change as required.
+#Simon discovered that some users were missing
+#I decided to run the report from the root of the domain
+
+$SearchBase = "OU=User,DC=example,DC=com"
+
+
+
+
+$AllADUsers = Get-ADUser  -searchbase $SearchBase -Filter * -Properties * #| Where-Object {$_.info -NE 'Migrated'} #ensures that updated users are never exported.
+
+$AllADUsers |
+Select-Object @{Label = "First Name";Expression = {$_.GivenName}},
+@{Label = "Last Name";Expression = {$_.Surname}},
+@{Label = "Display Name";Expression = {$_.DisplayName}},
+@{Label = "Logon Name";Expression = {$_.sAMAccountName}},
+@{Label = "Job Title";Expression = {$_.Title}},
+@{Label = "Company";Expression = {$_.Company}},
+@{Label = "Directorate";Expression = {$_.Description}},
+@{Label = "Department";Expression = {$_.Department}},
+@{Label = "Office";Expression = {$_.OfficeName}},
+@{Label = "Phone";Expression = {$_.telephoneNumber}},
+@{Label = "Email";Expression = {$_.Mail}},
+@{Label = "Manager";Expression = {%{(Get-AdUser $_.Manager -server $ADServer -Properties DisplayName).DisplayName}}},
+@{Label = "Account Status";Expression = {if (($_.Enabled -eq 'TRUE')  ) {'Enabled'} Else {'Disabled'}}}, # the 'if statement# replaces $_.Enabled
+@{Label = "Last LogOn Date";Expression = {$_.lastlogondate}},
+@{Label = "MemberOf";Expression = {%{(((get-aduser $_.samaccountname -Properties memberof).memberof) | Get-ADGroup).samaccountname}}} | 
+
+#Export CSV report
+
+Export-Csv -Path $csvfile -NoTypeInformation
