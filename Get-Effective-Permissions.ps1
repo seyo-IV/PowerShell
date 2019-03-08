f3b11f11fdef0a72f90e2cfe12c96a30971dc5d5
Ivanov <32060944+seyo-IV@users.noreply.github.com>
2019-03-08 11:19:44 +0100

    Fixed dublicates issue

--- Get-Effective-Permissions.ps1
+++ Get-Effective-Permissions.ps1
@@ -13,22 +13,36 @@ foreach($Dir in $PList)
         Write-Output "#######################################################################" | Out-File $Log -append
         Get-Item $Dir | select FullName | Out-File $Log -append
         $AclList = Get-Acl -Path $Dir -Filter Access | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "Domain\*"} | Select-Object IdentityReference
-        Get-Acl -Path $dir -Filter Access | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "Domain\*"} | Out-File $Log -append
+        Clear-Variable ACLFile
+        $ACLFile = Get-Acl -Path $dir -Filter Access | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "Domain\*"} | Select-Object IdentityReference, FileSystemRights
+        $ACLGroup = $ACLFile | Group-Object IdentityReference
+        $Singles = $ACLGroup.where({$_.count -eq 1}).group
+        $Duplicates = $ACLGroup.where({$_.count -gt 1})
+        $ItemizedDuplicates = $Duplicates | foreach {
+        [pscustomobject][ordered]@{"IdentityReference"=$_.Group.IdentityReference[0]; "FileSystemRights" = $_.Group.FileSystemRights -join ", "}
+            }
+        @($ItemizedDuplicates,$Singles) | Out-File $log -append
             foreach($Id in $AclList.IdentityReference.Value -replace 'Domain\\')
                 {
+                    if($ID -Like "*eckdadm*")
+                        {
+                        
+                        }
+                    else
+                        {
                     $ADGroup = Get-ADGroup $Id -Properties member | Select-Object -ExpandProperty member
                     Write-Output "`n" | Out-File $Log -append
                     Write-Output "Member of $Id `n
 ---------------------------------" | Out-File $Log -append
                     foreach ($Object in $ADGroup)
-                        {
-                            $Group      = Get-ADUser -filter * -SearchBase "$Object"
-                            if($Group -ne $null)
+		                {
+		                    $Group		= Get-ADUser -filter * -SearchBase "$Object"
+		                    if($Group -ne $null)
                                 {
-                                    $GrName     = $Group.Name
-                                    Write-Output "$GrName" | Out-File $Log -append
+                                    $GrName		= $Group.Name
+		                            Write-Output "$GrName" | Out-File $Log -append
                                 }
+		                }
                         } 
                 }
-Clear-Variable Obj, Object, Group, ADGroup, ACLList, GRName, Id
-    }
+Clear-Variable Object, Group, ADGroup, ACLList, GRName, Id
