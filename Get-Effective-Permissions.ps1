cbb09f8a333aad3635e9e935c2ab2fa017fbe1a5
Ivanov <32060944+seyo-IV@users.noreply.github.com>
2019-03-20 15:44:56 +0100

    Added scan to parent path

--- Get-Effective-Permissions.ps1
+++ Get-Effective-Permissions.ps1
@@ -1,20 +1,60 @@
+#------------------------------------------------------------------------ 
+# Author: Sergiy Ivanov (Im Auftrag der ECKD Service GmbH) 
+# Verwendungszweck: Dieses Script generiert Listen mit effecktiven berechtigungen. 
+# Datum: 20.03.20189 
+#------------------------------------------------------------------------ 
+# Errors  : Der Fehler bestand in? 
+# 
+# Changes : Folgendes wurde verändert 
+# 
+#------------------------------------------------------------------------ 
+# Variables 
+#------------------------------------------------------------------------ 
+
+import-module ActiveDirectory
+$i = 0
 $ErrorActionPreference = "SilentlyContinue"
-$Path   = "\\Server\Share\Logs\"
-$Log    = $Path + "Effective_Permissions" + ".log"
-$PPath = Read-Host "Enter Path to scan"
-$plist = Get-Childitem -Path $PPath -Recurse | ?{ $_.PSIsContainer } | Select-Object FullName
+$Path                  = "\\SERVER\Logs\"
+$log                  = $Path + "Effective_PermissionsV2" + ".log"
+$PPath                 = Read-Host "Enter Path to scan"
+
+$plist                 = @()
+$Folders               = $PPath -Split "\\"
+$PathList	           = $Folders | % { $i = 0 } { $Folders[0..$i] -Join "\" -Replace ":$", ":\"; $i++ }
+$Plist += $pathlist
+Write-Progress -Activity “Scaning Directory” -Status “Scanning” -PercentComplete 50
+try
+{
+$list = Get-Childitem -Path $PPath -Recurse | ?{ $_.PSIsContainer } | Select-Object FullName
+$list = $list.fullname
+$Plist += $list
+}
+catch {Write-Warning "Enter a correct path"}
+Write-Progress -Activity “Scaning Directory” -Status “Complete” -PercentComplete 100
 
 
+
+
+
+
+#------------------------------------------------------------------------ 
+# Foreach Loop 
+#------------------------------------------------------------------------
 foreach($Dir in $PList)
     {
-        $Dir = $Dir -replace "@{FullName=", "" -replace "}"
+    $UserList = @()
+    try{
+        #$Dir = $Dir -replace "@{FullName=", "" -replace "}"
         Resolve-Path -Path $Dir
         Write-Output "`n" | Out-File $log -append
         Write-Output "#######################################################################" | Out-File $Log -append
         Get-Item $Dir | select FullName | Out-File $Log -append
-        $AclList = Get-Acl -Path $Dir -Filter Access | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "Domain\*"} | Select-Object IdentityReference
+#------------------------------------------------------------------------ 
+# Collecting ACLs
+#------------------------------------------------------------------------
+        $AclList = Get-Acl -Path $Dir -Filter Access | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "DOMAINPREFIX\*"} | Select-Object IdentityReference
         Clear-Variable ACLFile
-        $ACLFile = Get-Acl -Path $dir -Filter Access | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "Domain\*"} | Select-Object IdentityReference, FileSystemRights
+        $ACLFile = Get-Acl -Path $dir -Filter Access | Select-Object -ExpandProperty Access | Where-Object {$_.IdentityReference -like "DOMAINPREFIX\*"} | Select-Object IdentityReference, FileSystemRights
         $ACLGroup = $ACLFile | Group-Object IdentityReference
         $Singles = $ACLGroup.where({$_.count -eq 1}).group
         $Duplicates = $ACLGroup.where({$_.count -gt 1})
@@ -22,27 +62,66 @@ foreach($Dir in $PList)
         [pscustomobject][ordered]@{"IdentityReference"=$_.Group.IdentityReference[0]; "FileSystemRights" = $_.Group.FileSystemRights -join ", "}
             }
         @($ItemizedDuplicates,$Singles) | Out-File $log -append
-            foreach($Id in $AclList.IdentityReference.Value -replace 'Domain\\')
+            foreach($Id in $AclList.IdentityReference.Value -replace 'DOMAINPREFIX\\')
                 {
-                    if($ID -Like "*adminprefix*") # I had admins which had permissions on files, that way i avoid them
+                    $idcheck = get-aduser $Id
+                    if($IDcheck -eq $true)
                         {
                         
                         }
                     else
-                        {
-                    $ADGroup = Get-ADGroup $Id -Properties member | Select-Object -ExpandProperty member
+{
+                        if($id -Like "*L_FS_DFS*")
+                                                    {
+                    $GrName = (Get-ADGroup $id -Properties member | Select-Object -ExpandProperty member | %{Get-ADGroup $_}).name
+                    $ADGroup = Get-ADGroup $GrName -Properties member | Select-Object -ExpandProperty member
                     Write-Output "`n" | Out-File $Log -append
-                    Write-Output "Member of $Id `n
----------------------------------" | Out-File $Log -append
+                   
+                   
+                    
+					Write-Host "`n" | Out-File $log -append
+                    Write-Output "Member of $Id : `n
+-----------------------------"
                     foreach ($Object in $ADGroup)
 		                {
-		                    $GetUser		= Get-ADUser -filter * -SearchBase "$Object"
-		                    if($GetUser -ne $null)
+		                    $GetName		= Get-ADUser -filter * -SearchBase "$Object"
+		                    if($GetName -ne $null)
                                 {
-                                    $Name		= $GetUser.Name
-		                            Write-Output "$Name" | Out-File $Log -append
+									$Name = $GetName.SamaccountName
+									Write-Output $name | Out-File $log -append
                                 }
+                                
+                                
 		                }
+                        }else{
+
+
+                    $ADGroup = Get-ADGroup $Id -Properties member | Select-Object -ExpandProperty member
+
+                   
+                    Write-Host "`n" | Out-File $log -append
+                    Write-Output "Member of $Id : `n
+-----------------------------" | Out-File $log -append
+                    foreach ($Object in $ADGroup)
+		                {
+		                    
+                            $GetName		= Get-ADUser -filter * -SearchBase "$Object"
+		                    if($GetName -ne $null)
+                                {
+                                    
+									$Name = $GetName.SamaccountName
+									Write-Output $name | Out-File $log -append
+                                    
+
+                                }
+                                
+                                
+		                }
+                           }
                         } 
                 }
-Clear-Variable Object, Group, ADGroup, ACLList, ACLFile, ACLGroup, Name, Id
+#Clear-Variable Object, Group, ADGroup, ACLList, ACLFile, ACLGroup, Name, Id
+$i++
+Write-Progress -Activity “Scaning folders” -Status “On $dir” -PercentComplete ($i / $plist.count*100)
+    }catch{$i++}
+    }
