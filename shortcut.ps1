c7b0009ad4c0baab1156acce0afde1bca19da107
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2019-08-21 16:42:35 +0200

    Add files via upload

---
+++ shortcut.ps1
@@ -0,0 +1,14 @@
+﻿[CmdletBinding ()]
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$ScriptName,
+		[string]$Directory = $PWD
+		)
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$PWD\$($ScriptName).lnk")
+$Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+$Arguments1 = "-windowstyle hidden -Noninteractive -ExecutionPolicy Bypass –Noprofile -file"
+$Arguments2 = "$($Directory)\$($ScriptName).ps1"
+$Shortcut.Arguments = $Arguments1 + " " + $Arguments2
+$Shortcut.WorkingDirectory = $Directory
+$Shortcut.Save()
