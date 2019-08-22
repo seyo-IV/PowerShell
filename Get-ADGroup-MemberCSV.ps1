55c597395ee8c3c5c6520d85732a90114d885a2d
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2019-08-22 14:17:38 +0200

    Add files via upload

---
+++ Get-ADGroup-MemberCSV.ps1
@@ -0,0 +1,38 @@
+
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory=$True)]
+	[string]$ListFile,
+	[Parameter(Mandatory=$True)]
+	[string]$CSVPAth
+	)
+$groups = Get-Content $ListFile
+$ObjectList =  @()
+foreach($group in $groups){
+$Groupname = $group
+$GroupMember = Get-ADGroup -Identity $group -Properties member | Select-Object -ExpandProperty member
+$data = @()
+	foreach ($Member in $GroupMember)
+		{
+		$GMember	= Get-ADUser -Filter * -SearchBase $Member -Properties *
+		$Name = $GMember.Name
+		$Username = $GMember.SamAccountName
+		$Email = $GMember.mail
+		$Address = $GMember.PhysicalDeliveryOfficeName
+		$Location = $GMember.DistinguishedName
+		$Enabled = $GMember.Enabled
+		Write-Host $Address
+$list = New-Object PSCustomObject
+$list | Add-Member -type NoteProperty -Name "Group" -value $Groupname
+$list | Add-Member -type NoteProperty -Name "Name" -value $Name
+$list | Add-Member -type NoteProperty -Name "SamAccountName" -value $Username
+$list | Add-Member -type NoteProperty -Name "Email" -value $Email
+$list | Add-Member -type NoteProperty -Name "Address" -value $Address
+$list | Add-Member -type NoteProperty -Name "Location" -value $Location
+$list | Add-Member -type NoteProperty -Name "Enabled" -value $Enabled
+$data += $list
+$ObjectList += $data
+		}
+
+}
+$ObjectList | Export-Csv $CSVPAth -NoTypeInformation -Delimiter ';' -Encoding UTF8
