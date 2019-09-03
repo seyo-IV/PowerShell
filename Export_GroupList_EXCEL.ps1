4779d2770f931b9b9e617fbacdf4d8c5c44debc3
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2019-09-03 10:14:05 +0200

    Update Export_GroupList_EXCEL.ps1

--- Export_GroupList_EXCEL.ps1
+++ Export_GroupList_EXCEL.ps1
@@ -1,10 +1,40 @@
+#requires -version 3 -module ActiveDirectory, ImportExcel
+<#
+.SYNOPSIS
+  This script exports Active directory User or Groups to a EXCEL file.
+  
+.DESCRIPTION
+  Export user membership, group membership or group member ot a .xlsx file.
+  
+.PARAMETER LogName
+  Name for the EXCEL file.
+  
+ .PARAMETER ListPath
+ Path to textfile with the group names. Default is current directory.
+    
+.INPUTS
+  None.
+  
+.OUTPUTS
+  \\SERVER\$logname.xlsx
+  
+.NOTES
+  Version:        1.0
+  Author:         Sergiy Ivanov
+  Creation Date:  03.09.2019
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  Export_GroupList.ps1 -LogName "Export_groups" -ListPath "C:\logs\groups.txt"
+#>
+
 [CmdletBinding()]
 param(
 
 	[Parameter(Mandatory=$True,
 	ValueFromPipeline=$True,
 	ValueFromPipelineByPropertyName=$True)]
-	[string]$ListPath,
+	[string]$ListPath = $PWD,
 	[Parameter(Mandatory=$True,
 	ValueFromPipeline=$True,
 	ValueFromPipelineByPropertyName=$True)]
