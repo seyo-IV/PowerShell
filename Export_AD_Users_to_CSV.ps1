7fdfc4104673d6ace7d7a80ac0a020b7a9ff5e57
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2019-09-03 10:01:32 +0200

    Added some Info

--- Export_AD_Users_to_CSV.ps1
+++ Export_AD_Users_to_CSV.ps1
@@ -4,15 +4,34 @@
 # CREATED 				: 08-08-2014 
 # UPDATED 				: 12-02-2019 
 # COMMENT 				: This script exports Active Directory user
-#           to a a csv file.
+#                   to a a csv file.
 ###########################################################
-
-
-#Define location of my script variable
-#the -parent switch returns one directory lower from directory defined. 
-#below will return up to ImportADUsers folder 
-#and since my files are located here it will find it.
-#It failes withpout appending "*.*" at the end
+#requires -version 3 -module ActiveDirectory
+<#
+.SYNOPSIS
+  This script exports Active directory User to a CSV file, i added a field called MemberOf, which exports also the membership of a user.
+  
+.DESCRIPTION
+  Export AD-User to a CSV.
+  
+.PARAMETER SearchBase
+  Set the OU in which the filter will search for users.
+    
+.INPUTS
+  None.
+  
+.OUTPUTS
+  Creates the CSV in the current directory.
+  
+.NOTES
+  Version:        1.0
+  Author:         Sergiy Ivanov
+  Creation Date:  12.02.2019
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  Export_AD_Users_to_CSV.ps1 -SearchBase "OU=User,DC=example,DC=com"
+#>
 
 $path = Split-Path -parent $PWD
 
@@ -28,13 +47,11 @@ $csvfile = $path + "\ALLADUsers_$logDate.csv"
 #import the ActiveDirectory Module
 
 Import-Module ActiveDirectory
-
-
-#Sets the OU to do the base search for all user accounts, change as required.
-#Simon discovered that some users were missing
-#I decided to run the report from the root of the domain
-
-$SearchBase = "OU=User,DC=example,DC=com"
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory=$True]
+	[string]$SearchBase = (Get-ADDomain).DistinguishedName
+  )
 
 $ErrorActionPreference = "SilentlyContinue"
 
