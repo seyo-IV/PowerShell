a1894cefe4d239d57b769c84d38be7bde6f8b54b
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2020-05-19 18:48:00 +0200

    Update Setup_PS.ps1

--- Security-Tool/0_code/Setup_PS.ps1
+++ Security-Tool/0_code/Setup_PS.ps1
@@ -1,4 +1,4 @@
-﻿#
+#
 Function Check-PS-RL {
 $Modules = (Get-Module -ListAvailable).Name
 if($Modules -contains "PSReadLine")
@@ -17,8 +17,11 @@ else
 
 if((Check-PS-RL) -eq $False)
     {
+    $proxy = Get-Content $PSScriptRoot\proxy.conf
+    $pAddress= $proxy[0]
+    $pPort = $proxy[1]
     ## setup Proxy
-    [system.net.webrequest]::defaultwebproxy = new-object system.net.webproxy('http://YOURPROXY:PORT')
+    [system.net.webrequest]::defaultwebproxy = new-object system.net.webproxy('http://$pAddress:$pPort')
     [system.net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
     [system.net.webrequest]::defaultwebproxy.BypassProxyOnLocal = $true
     ## install PSReadLine
