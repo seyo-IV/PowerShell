6cc6eb1dea10492b0f92551afa8143af8439adb6
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2020-05-19 18:47:39 +0200

    Update PreRequiments.ps1

--- Security-Tool/0_code/PreRequiments.ps1
+++ Security-Tool/0_code/PreRequiments.ps1
@@ -4,6 +4,9 @@ if($proxyset.proxyEnable -eq 1)
 Write-Host "Proxy is enabled, please provide some info" -ForegroundColor Yellow
 $pAddress = Read-Host "Proxy Address"
 $pPort = Read-Host "Proxy Port"
+$pAddress | Out-File $PSScriptRoot\proxy.conf
+$pPort | Out-File $PSScriptRoot\proxy.conf -Append
+
 ## Set Proxy
 [system.net.webrequest]::defaultwebproxy = new-object system.net.webproxy('http://pAddress:pPort')
 [system.net.webrequest]::defaultwebproxy.credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
