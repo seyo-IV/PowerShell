cd5def28b25eb4210dbe35bf342c2eaeb173613e
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2020-05-19 19:44:43 +0200

    Delete Generate_Shortcut_Desktop.ps1

--- Security-Tool/0_code/Generate_Shortcut_Desktop.ps1
+++
@@ -1,8 +0,0 @@
-﻿$Directory = "C:\Users\Public\Desktop"
-$Path = (get-item $PSScriptRoot).parent.parent.FullName
-Remove-Item -Path "$Directory\Security-Tool.lnk" -Force -ErrorAction SilentlyContinue
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$Directory\Security-Tool.lnk")
-$Shortcut.TargetPath = "$Path\Security-Tool\Security-Tool.exe"
-$Shortcut.WorkingDirectory = $Directory
-$Shortcut.Save()
