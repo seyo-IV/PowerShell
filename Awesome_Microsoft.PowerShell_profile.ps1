0da28b9654cde7d6dadaccc2420ce5811b891a2e
Sergiy Ivanov <32060944+seyo-IV@users.noreply.github.com>
2020-08-11 10:24:58 +0200

    Update Awesome_Microsoft.PowerShell_profile.ps1

--- Awesome_Microsoft.PowerShell_profile.ps1
+++ Awesome_Microsoft.PowerShell_profile.ps1
@@ -128,25 +128,31 @@ param(
 	[string]$Server
 	)
  
-$CPUInfo = Get-WmiObject -ComputerName $server Win32_Processor
-$OSInfo = Get-WmiObject -ComputerName $server Win32_OperatingSystem
-
-$infoObject = New-Object PSObject -Property {
+#Run the commands concurrently for each server in the list 
+$CPUInfo = Get-WmiObject -ComputerName $server Win32_Processor #Get CPU Information 
+$OSInfo = Get-WmiObject -ComputerName $server Win32_OperatingSystem #Get OS Information
+#GEt IPv4
+$IPv4 = (Test-Connection -ComputerName $server).IPV4Address 
+#Get Memory Information. The data will be shown in a table as MB, rounded to the nearest second decimal. 
+$OSTotalVirtualMemory = [math]::round($OSInfo.TotalVirtualMemorySize / 1MB, 2) 
+$OSTotalVisibleMemory = [math]::round(($OSInfo.TotalVisibleMemorySize  / 1MB), 2) 
+$PhysicalMemory = Get-WmiObject -ComputerName $server CIM_PhysicalMemory | Measure-Object -Property capacity -Sum | % {[math]::round(($_.sum / 1GB),2)} 
+$infoObject = New-Object PSObject 
 #The following add data to the infoObjects. 
-ServerName = $CPUInfo.SystemName 
-CPU_Name = $CPUInfo.Name 
-CPU_Description = $CPUInfo.Description 
-CPU_Manufacturer = $CPUInfo.Manufacturer 
-CPU_NumberOfCores = $CPUInfo.NumberOfCores 
-CPU_L2CacheSize = $CPUInfo.L2CacheSize 
-CPU_L3CacheSize = $CPUInfo.L3CacheSize 
-CPU_SocketDesignation = $CPUInfo.SocketDesignation 
-OS_Name = $OSInfo.Caption 
-OS_Version = $OSInfo.Version 
-TotalPhysical_Memory_GB = Get-WmiObject -ComputerName $server CIM_PhysicalMemory | Measure-Object -Property capacity -Sum | % {[math]::round(($_.sum / 1GB),2)} 
-TotalVirtual_Memory_MB = [math]::round($OSInfo.TotalVirtualMemorySize / 1MB, 2) 
-TotalVisable_Memory_MB = [math]::round(($OSInfo.TotalVisibleMemorySize  / 1MB), 2)
-}
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "ServerName" -value $CPUInfo.SystemName 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "IPv4" -value $IPv4
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_Name" -value $CPUInfo.Name 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_Description" -value $CPUInfo.Description 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_Manufacturer" -value $CPUInfo.Manufacturer 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_NumberOfCores" -value $CPUInfo.NumberOfCores 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_L2CacheSize" -value $CPUInfo.L2CacheSize 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_L3CacheSize" -value $CPUInfo.L3CacheSize 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "CPU_SocketDesignation" -value $CPUInfo.SocketDesignation 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "OS_Name" -value $OSInfo.Caption 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "OS_Version" -value $OSInfo.Version 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalPhysical_Memory_GB" -value $PhysicalMemory 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalVirtual_Memory_MB" -value $OSTotalVirtualMemory 
+Add-Member -inputObject $infoObject -memberType NoteProperty -name "TotalVisable_Memory_MB" -value $OSTotalVisibleMemory 
 $info = $infoObject | Select-Object * -ExcludeProperty PSComputerName, RunspaceId, PSShowComputerName 
 return $info
 
